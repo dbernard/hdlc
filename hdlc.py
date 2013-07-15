634dc83b7ae1da8d25077a424b3690e7f41d40db
John Szakmeister <john@szakmeister.net>
2013-07-15 12:49:57 -0400

    hdlc: implement an out-of-sync state and eliminate the start frame state
    This also changed how completed frames are sent back, since we want to
    be able to reset the frame buffer.

--- hdlc.py
+++ hdlc.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class State(object):
     def __init__(self, name):
         self._name = name
@@ -12,8 +15,8 @@ class State(object):
         return hash(repr(self))
 
 
+OUT_OF_SYNC = State("Out-of-Sync")
 IDLE = State("Idle")
-START_FRAME = State("Start Frame")
 GET_FRAME = State("Get Frame")
 GET_ESC = State("Get Esc")
 
@@ -42,21 +45,20 @@ class Receiver(object):
     States
     ------
 
+    OUT_OF_SYNC:
+      0x7E -> GET_FRAME
+      * -> OUT_OF_SYNC
     IDLE:
       0xFF -> IDLE
       0x7E -> START_FRAME
       * -> error, IDLE
-    START_FRAME
-      0x7E -> EMPTY
-      START_FRAME
-      * -> GET_FRAME
-    GET_FRAME
-      0x7E -> verify, START_FRAME
+    GET_FRAME:
+      0x7E -> verify, queue, GET_FRAME
       0x7D -> GET_ESC
       * -> save, GET_FRAME
-    GET_ESC
-      0x7E -> error, START_FRAME
-      0x7D -> error, IDLE
+    GET_ESC:
+      0x7E -> error, OUT_OF_SYNC
+      0x7D -> error, OUT_OF_SYNC
       * -> save XOR 0x20, GET_FRAME
     '''
 
@@ -65,11 +67,11 @@ class Receiver(object):
         self.state = IDLE
         self.statistics = create_statistics()
         self.frame = []
-        self.frame_complete = False
+        self.completed_frames = deque()
         self.frame_error = False
         self.state_handler = {
+            OUT_OF_SYNC: self.process_out_of_sync,
             IDLE: self.process_idle,
-            START_FRAME: self.process_start,
             GET_FRAME: self.process_get,
             GET_ESC: self.process_esc,
         }
@@ -77,46 +79,56 @@ class Receiver(object):
     def _read(self):
         return self.device.read(1)
 
+    def set_state(self, next_state):
+        current_state = self.state
+
+        if next_state == GET_FRAME:
+            if current_state != GET_ESC:
+                self.frame = []
+                self.frame_error = False
+
+        self.state = next_state
+
+    def process_out_of_sync(self, c):
+        if c == HDLC_FLAG:
+            self.state = GET_FRAME
+
     def process_idle(self, c):
         if c == HDLC_IDLE:
             self.state = IDLE
         elif c == HDLC_FLAG:
-            self.state = START_FRAME
+            self.state = GET_FRAME
         else:
             self.statistics['unframed'] += 1
 
-    def process_start(self, c):
-        if c == HDLC_FLAG:
-            self.statistics['empty'] += 1
-        elif c == HDLC_IDLE:
-            self.state = IDLE
-        elif c == HDLC_ESC:
-            self.state = GET_ESC
-            self.frame = []
-        else:
-            self.state = GET_FRAME
-            self.frame = [c]
-
     def process_get(self, c):
         if c == HDLC_FLAG:
-            self.verify_frame()
+            if len(self.frame) == 0:
+                self.statistics['empty'] = 1
+            else:
+                if self.verify_frame():
+                    self.completed_frames.append(self.frame)
+            self.set_state(GET_FRAME)
         elif c == HDLC_ESC:
-            self.state = GET_ESC
+            self.set_state(GET_ESC)
         else:
             self.frame.append(c)
 
     def process_esc(self, c):
         if c == HDLC_FLAG:
             self.statistics['escaped_flag'] += 1
-            self.state = START_FRAME
+            self.statistics['invalid'] += 1
+            self.set_state(GET_FRAME)
+
         elif c == HDLC_ESC:
             self.statistics['double_escape'] += 1
-            self.state = IDLE
+            self.statistics['invalid'] += 1
+            self.set_state(OUT_OF_SYNC)
         else:
             value = ord(c)
             decoded = value ^ HDLC_ESC_MOD
             self.frame.append(chr(decoded))
-            self.state = GET_FRAME
+            self.set_state(GET_FRAME)
 
     def process_state(self, c):
         self.state_handler[self.state](c)
@@ -125,21 +137,24 @@ class Receiver(object):
         if len(self.frame) < 4:
             self.statistics['invalid'] += 1
             self.frame_error = True
-            return
-        self.frame_complete = True
+            return False
+        return True
 
     def get(self):
         self.frame_complete = False
         self.frame_error = False
         while True:
             c = self._read()
+
             if not c:
                 self.statistics['timeout'] += 1
                 return None
+
             # Add to byte count for every valid byte
             self.statistics['bytes'] += 1
             self.process_state(c)
-            if self.frame_complete:
-                return ''.join(self.frame)
+
+            if self.completed_frames:
+                return ''.join(self.completed_frames.popleft())
             if self.frame_error:
                 return None
